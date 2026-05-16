a4f7e7a7fb4f699aebb45b1bec8460a7ffd01b4f
ExoticN9 <mjnoonk334@gmail.com>
2026-05-16 10:06:39 +0300

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,35 +1,63 @@
 export function exportExcel() {
 
-    const wb = XLSX.utils.book_new();
+    const wb =
+      XLSX.utils.book_new();
 
-    const data = [];
+    const data = [
 
-    document
-        .querySelectorAll('input, textarea')
-        .forEach(el => {
+      ['تقرير تشغيل محطة الحاير'],
 
-            data.push([
-                el.id,
-                el.value
-            ]);
-        });
+      [],
+
+      ['التاريخ',
+      document.getElementById('reportDate').value],
+
+      ['الاستهلاك',
+      document.getElementById('power').value],
+
+      [],
+
+      ['إنتاج الحاير',
+      document.getElementById('h_prod').value],
+
+      ['تصدير الحاير',
+      document.getElementById('h_export').value],
+
+      ['إنتاج منفوحة',
+      document.getElementById('m_prod').value],
+
+      ['تصدير منفوحة',
+      document.getElementById('m_export').value],
+
+      ['إنتاج الشميسي',
+      document.getElementById('s_prod').value],
+
+      ['تصدير الشميسي',
+      document.getElementById('s_export').value],
+
+      [],
+
+      ['معد التقرير',
+      document.getElementById('reporter_name').value],
+
+      ['المشرف',
+      document.getElementById('supervisor_name').value]
+
+    ];
 
     const ws =
-        XLSX.utils.aoa_to_sheet(data);
+      XLSX.utils.aoa_to_sheet(data);
 
     ws['!dir'] = 'rtl';
 
     XLSX.utils.book_append_sheet(
-        wb,
-        ws,
-        'التقرير'
+      wb,
+      ws,
+      'التقرير'
     );
 
-    const date =
-        document.getElementById('reportDate').value;
-
     XLSX.writeFile(
-        wb,
-        `تقرير_${date}.xlsx`
+      wb,
+      `تقرير_${document.getElementById('reportDate').value}.xlsx`
     );
 }
